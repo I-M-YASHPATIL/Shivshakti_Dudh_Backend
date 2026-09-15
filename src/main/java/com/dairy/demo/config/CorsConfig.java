eaed3058fc7d2a2f0d5600a14edc077e7428851e
Yash Patil <yash.patil1609@gmail.com>
2026-09-15 18:02:31 +0530

    Added frontend depl link in CorsConfig.java

--- src/main/java/com/dairy/demo/config/CorsConfig.java
+++ src/main/java/com/dairy/demo/config/CorsConfig.java
@@ -14,7 +14,11 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:5173")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "http://localhost:5173",
+                                "https://shivshakti-dudh-frontend.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .maxAge(3600);
